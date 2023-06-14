a9f84dab78e18041242b103a040670f81dc6f178
unknown <noorazzam19@gmail.com>
2023-06-14 09:11:31 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import MyNavbar from "./components/navbar";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+    return <div>
+        <MyNavbar />
+    </div>
+}
